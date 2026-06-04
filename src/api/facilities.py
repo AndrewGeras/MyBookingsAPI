aa92b33bb7ad3396cce2e48e7cb9cbd118c57abc
Andrey Gerasimov <gerasimov.andrew4work@gmail.com>
2026-06-04 12:25:34 +0200

    added simple caching with redis to facilities

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -1,17 +1,27 @@
+import json
 from typing import Annotated
 
 from fastapi import APIRouter, Body
 from starlette.status import HTTP_201_CREATED
 
+from src.init import redis_manager
 from src.api.dependencies import DBDep
 from src.schemas.facilities import FacilityAdd
 
 
 router = APIRouter(prefix="/facilities", tags=["Удобства"])
+
+
 @router.get("",
             description="<h2>Ручка для получения списка всех удобств</h2>")
 async def get_all_facilities(db: DBDep):
-    return await db.facilities.get_all()
+    facilities_from_cache = await redis_manager.get("facilities")
+    if facilities_from_cache:
+        return json.loads(facilities_from_cache)
+    facilities = await db.facilities.get_all()
+    facilities_for_cache = json.dumps([facility.model_dump() for facility in facilities])
+    await redis_manager.set("facilities", facilities_for_cache, expire=60)
+    return facilities
 
 
 @router.post("",
--- src/main.py
+++ src/main.py
@@ -1,18 +1,29 @@
 import sys
 from pathlib import Path
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from uvicorn import run
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+from src.init import redis_manager
 from src.api.hotels import router as hotels_router
 from src.api.auth import router as users_router
 from src.api.rooms import router as rooms_router
 from src.api.bookings import router as bookings_router
 from src.api.facilities import router as facilities_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    "контекстный менеджер управляющий подключением к Redis"
+    await redis_manager.connect()  # при старте приложения
+    yield
+    await redis_manager.close()  # при остановке/перезапуске приложения
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 app.include_router(users_router)
